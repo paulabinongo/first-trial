93f4bd59067e53088c1740851d34e9726d977aed
paulabinongo <paula.binongo@98labs.com>
2020-04-20 13:14:05 +0800

    Updates

---
+++ precache-manifest.f8a71241a5b9eeefedb53fae38dc7d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b12896049fbf6ac20efff939091d792",
+    "url": "/first-trial/index.html"
+  },
+  {
+    "revision": "e4eb75a1ced84b3bcc45",
+    "url": "/first-trial/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33bf2c196ace0f1bd743",
+    "url": "/first-trial/static/js/2.d3c49dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-trial/static/js/2.d3c49dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4eb75a1ced84b3bcc45",
+    "url": "/first-trial/static/js/main.2e729b6c.chunk.js"
+  },
+  {
+    "revision": "7797b29cd30d1096c11d",
+    "url": "/first-trial/static/js/runtime-main.094b9951.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
